6d783c4cf2dbd2c83e477afeee7125b96d2c9339
agta <angeloabrita@gmail.com>
2024-12-26 07:19:37 -0300

    :broom: cleanup: refatorando navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,42 @@
 import React from 'react';
-    import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
- import useAuth from '../hooks/useAuth';
- import { Link, useNavigate } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import useAuth from '../hooks/useAuth';
+import { Link, useNavigate } from 'react-router-dom';
 
- const AppNavbar = () => {
+const AppNavbar = () => {
    const { user, isAuthenticated, logout } = useAuth();
-      console.log(user);
-       const navigate = useNavigate();
-       const handleLogout = () => {
-        logout();
+   console.log(user);
+   const navigate = useNavigate();
+   const handleLogout = () => {
+      logout();
       navigate('/auth')
-  };
+   };
 
 
-      return (
+   return (
       <Navbar bg="light" expand="lg">
-           <Navbar.Brand as={Link} to="/dashboard">Gympass App</Navbar.Brand>
-           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-           <Navbar.Collapse id="basic-navbar-nav">
+         <Navbar.Brand as={Link} to="/dashboard">Gympass App</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-                   {isAuthenticated ? (
-                    <>
-                        <NavDropdown align="end" title={user?.name || 'Usuário'} id="basic-nav-dropdown">
-                                  <NavDropdown.Item as={Link} to="/profile">Meu Perfil</NavDropdown.Item>
-                       {user?.role === 'admin' && ( //show register if its a admin
-                        <NavDropdown.Item as={Link} to="/gyms/create">Cadastrar Academia</NavDropdown.Item>
-                           )}
-                             <NavDropdown.Divider />
-                                  <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
-                          </NavDropdown>
-                     </>
-             ) : (
-                         <Button as={Link} to="/auth" variant="outline-primary">Login</Button>
-                      )}
-                   </Nav>
-                </Navbar.Collapse>
-        </Navbar>
-       );
-     };
+               {isAuthenticated ? (
+                  <>
+                     <NavDropdown align="end" title={user?.name || 'Usuário'} id="basic-nav-dropdown">
+                        <NavDropdown.Item as={Link} to="/profile">Meu Perfil</NavDropdown.Item>
+                        {user?.role === 'admin' && ( //show register if its a admin
+                           <NavDropdown.Item as={Link} to="/gyms/create">Cadastrar Academia</NavDropdown.Item>
+                        )}
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+                     </NavDropdown>
+                  </>
+               ) : (
+                  <Button as={Link} to="/auth" variant="outline-primary">Login</Button>
+               )}
+            </Nav>
+         </Navbar.Collapse>
+      </Navbar>
+   );
+};
 
- export default AppNavbar;
+export default AppNavbar;
